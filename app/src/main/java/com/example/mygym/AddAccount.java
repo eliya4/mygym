1bc5c7dfd08c036bdb85964071b231c83f02676e
eliya <eliyacaspi4@gmail.com>
2024-12-30 15:34:10 +0200

    סיום המאין האקטביטי פלוס הוספת דף להוספת חשבון

--- app/src/main/java/com/example/mygym/AddAccount.java
+++ app/src/main/java/com/example/mygym/AddAccount.java
@@ -3,14 +3,12 @@ package com.example.mygym;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class AddAccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_add_account);
     }
 }
